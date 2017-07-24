f6c9086e7c5c4f9fe9c7910fb9304de63fc60000
Sébastien CAPPELAERE <sebcappelaere@sfr.fr>
2017-07-24 22:14:47 +0200

    Affichage liste des compagnies

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Company;
 use AppBundle\Form\CompanyType;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -40,19 +41,32 @@ class DefaultController extends Controller
 
         //Persistence uniquement si le formulaire est soumis et si les tests de validation sont tous passés
         if ($form->isSubmitted() && $form->isValid()){
-            //Persistence de l'entité Company
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($company);
-            $em->flush();
+            try{
+                //Persistence de l'entité Company
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($company);
+                $em->flush();
 
-            //Ajout d'un message flash de création
-            $this->addFlash("info", "La compagnie a bien été créée");
+                //Ajout d'un message flash de création
+                $this->addFlash("info", "La compagnie a bien été créée");
+
+            } catch (UniqueConstraintViolationException $ex){
+                $this->addFlash("error","Cette compagnie existe déjà");
+            }
+
         }
+
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Company');
+        $listCompany = $repo->createQueryBuilder('c')
+            ->orderBy('c.name')->getQuery()->getResult();
 
         //Affichage de la vue avec le formulaire
         return $this->render(
             "newCompany.html.twig",
-            ["companyForm" => $form->createView()]
+            [
+                "companyForm" => $form->createView(),
+                "listCompany" => $listCompany
+            ]
         );
 
     }
