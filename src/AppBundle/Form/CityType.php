e8f9c967714fa0123c720e8e7db73bf03bf090b5
Sébastien CAPPELAERE <sebcappelaere@sfr.fr>
2017-07-25 23:18:54 +0200

    Ajout et affichage liste des villes

---
+++ src/AppBundle/Form/CityType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CityType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class, ["label" => "Nom de la ville"])
+            ->add('zipCode', TextType::class, ["label" => "Code postal"])
+            ->add('submit', SubmitType::class, ["label" => "Ajouter"]);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\City'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_city';
+    }
+
+
+}
